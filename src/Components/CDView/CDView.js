71d5ad3c4b024f6ee872014b09ae7b70543a77f6
Kemény Vince <93611221+KVince002@users.noreply.github.com>
2023-09-26 11:48:40 +0200

    Add initial CDView component

---
+++ src/Components/CDView/CDView.js
@@ -0,0 +1,11 @@
+// When the user presses the button it will become full screen showing the full info about the CD
+const CDView = (props) => {
+
+	return(
+		<>
+			<p>Views of the cd</p>
+		</>
+	)
+}
+
+export default CDView
