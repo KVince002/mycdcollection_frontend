d6290ebc8df3868dee736276a5e95adfd570a848
Kemény Vince <93611221+KVince002@users.noreply.github.com>
2023-09-26 12:52:30 +0200

    "/" route added for <Homepage/>

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,14 @@
 import '../App.css';
 import Homepage from "../Components/Homepage/Homepage";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 function App() {
   return (
-<Homepage />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' index element={<Homepage/>}></Route>
+        </Routes>
+      </BrowserRouter>
   );
 }
 
