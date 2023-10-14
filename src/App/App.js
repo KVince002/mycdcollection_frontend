de9ce7396e7d7ece63f56919f146d53ac4892892
Kemény Vince <93611221+KVince002@users.noreply.github.com>
2023-10-14 23:39:24 +0200

    Route added for viewing cds

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,18 @@
 import '../App.css';
 import Homepage from "../Components/Homepage/Homepage";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
+import CDView from "../Components/CDView/CDView";
 
 function App() {
   return (
       <BrowserRouter>
         <Routes>
-          <Route path='/' index element={<Homepage/>}></Route>
+          <Route path='/' index element={<Homepage/>}>
+{/*
+              Importing CDView for the route}
+*/}
+              <Route path="disk/:id" element={<CDView/>}></Route>
+          </Route>
         </Routes>
       </BrowserRouter>
   );
