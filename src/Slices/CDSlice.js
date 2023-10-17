00c9bafec406495de7add3690a80964f667aa24b
Kemény Vince <93611221+KVince002@users.noreply.github.com>
2023-10-17 02:20:43 +0200

    Specific cd getter function added and updated initialstate

--- src/Slices/CDSlice.js
+++ src/Slices/CDSlice.js
@@ -1,41 +1,64 @@
 // slice for the cds
 import {createSlice} from "@reduxjs/toolkit"
 
-  // initialState
-  const initialState={
-    cds: [],
-    status: "idle" | "pending" | "fulfilled" | "rejected",
-    error: null
-  }
-
-  // async thunk
-  // todo: Comment
-export async function fetchCDs(dispatch, getState){
-  await fetch("http://localhost:3001/fetchallcds")
-      .then((res) => res.json())
-      .then((data) => dispatch({type: "CDs/addCDs", payload: data}))
-      .catch((err) => console.log(err))
+// initialState
+const initialState={
+	// all the cds
+  cds: [],
+	// specific cd
+	cdSpec: [],
+	// status *unused yet*
+  status: "idle" | "pending" | "fulfilled" | "rejected",
+	// error *unused yet*
+  error: null
 }
 
-  // slice
-  // todo: "normalize"
-  const slice = createSlice({
-    name: "CDs",
-    initialState,
-    reducers: {
-      addCDs: (state, action) => {
-        state.cds = action.payload;
-      },
-      manipulateCDs: {
-        reducer: (state, action) => {
-          console.log("Inside CDSlice > slice > reducers > manipulatecds")
-          console.log(action.payload)
-          state.cds += action.payload
-        },
-        prepare: (value) => ({payload:value || "nothing arrived"})
-      }
-    }
-  })
+// async thunk
+// todo: Comment
+/*
+* This is an async function that calls (dispatch) and action while fetching data form the server
+* */
+export async function fetchCDs(dispatch, getState){
+	// fetching from the server
+  await fetch("http://localhost:3001/fetchallcds")
+	  // response to JSON
+      .then((res) => res.json())
+	  // dispatch the addCDs action and give the data payload
+      .then((data) => dispatch({type: "CDs/addCDs", payload: data}))
+	  // catch the error and console it
+      .catch((err) => console.error(err))
+}
+
+// Accrue a specific song info from server
+export async function fetchCDInfo(dispatch, getState, cdItem) {
+	await fetch("http://localhost:3001/cds/"+cdItem)
+		.then((res)=> res.json())
+		.then((data)=> dispatch({type: "CDs/redirectCDView", payload: data}))
+		.catch((error)=> console.error(error))
+}
+
+// slice
+// todo: "normalize"
+const slice = createSlice({
+  name: "CDs",
+  initialState,
+  reducers: {
+    addCDs: (state, action) => {
+		console.log("Inside CDSlice > slice > reducers > addCDs")
+	    state.cds = action.payload;
+    },
+	  redirectCDView: (state, action)=> {
+		console.log("Inside CDSlice > slice > reducers > redirectCDView");
+		  /*
+		  * // Todo: Get the right payload and with query and pass the information to the CDView the render it!
+		  * */
+		// reset the cdSpec array first
+		  state.cdSpec = [];
+		  // give the data to it
+		  state.cdSpec = action.payload;
+	  }
+  }
+})
 
 console.log(slice)
 
