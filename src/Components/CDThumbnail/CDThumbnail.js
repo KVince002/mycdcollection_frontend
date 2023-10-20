1bd6b62d05241cb6eae99c2a77920aae9cc62a31
Kemény Vince <93611221+KVince002@users.noreply.github.com>
2023-10-20 02:42:40 +0200

    CDView added. No style. ⚠️ Reload brokes the page ⚠️.

--- src/Components/CDThumbnail/CDThumbnail.js
+++ src/Components/CDThumbnail/CDThumbnail.js
@@ -1,10 +1,13 @@
 // This is thumbnail for the Homepage's grid
+import {useNavigate} from "react-router";
+
 const CDThumbnail = (props) => {
-	const {artist, title, cover, key} = props;
+	const {artist, title, cover, key, id} = props;
+	const navigate = useNavigate()
 
 	return (
 		<div className="flex flex-col justify-center">
-			<img key={key} className="order-first rounded-md w-24 md:w-44 lg:w-60 hover:shadow-lg hover:shadow-ctp-sapphire" src={cover} alt={`Cover art for ${artist} - ${title} album`}/>
+			<img key={key} className="order-first rounded-md w-24 md:w-44 lg:w-60 hover:shadow-lg hover:shadow-ctp-sapphire" src={cover} alt={`Cover art for ${artist} - ${title} album`} onClick={()=> navigate(`/disk/${id}`)}/>
 			<h2 className="text-ctp-blue">{title}</h2>
 			<h3 className="text-ctp-lavender">{artist}</h3>
 		</div>
--- src/Components/CDView/CDView.js
+++ src/Components/CDView/CDView.js
@@ -1,11 +1,30 @@
 // When the user presses the button it will become full screen showing the full info about the CD
+// ColorThief import
+import {useParams} from "react-router";
+import {useSelector} from "react-redux";
+
 const CDView = (props) => {
+	// props
+	const {id} = useParams()
+	console.log(`Id props ${id}`)
+
+	// select the specified cd from all the cds
+	const CDs = useSelector((state) => state.CDSlice.cds)
+	const actualCD = CDs.filter((item) => item.id == id)
+	console.table(actualCD)
 
 	return(
-
-		<>
-			<p>Views of the cd</p>
-		</>
+		<div>
+			<div>
+			<img src={actualCD[0].cover} alt={`This is a cover of ${actualCD[0].title} by ${actualCD[0].artist}`} id={"coverImg"} loading={"eager"}/>
+			</div>
+			<div>
+			<h1>{actualCD[0].title}</h1>
+			<h2>{actualCD[0].artist}</h2>
+			<h3>{actualCD[0].releasedate}</h3>
+			<h3>{actualCD[0].genre}</h3>
+			</div>
+		</div>
 	)
 }
 
