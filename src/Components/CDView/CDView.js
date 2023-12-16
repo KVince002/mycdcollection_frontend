511660006aa71dea663c8e60d355085638864406
Kemény Vince <kemeny.vince@outlook.hu>
2023-12-16 14:25:12 +0100

    useEffect and a bit normalize

--- src/Components/CDView/CDView.js
+++ src/Components/CDView/CDView.js
@@ -4,6 +4,7 @@ import {useParams} from "react-router";
 import {useDispatch, useSelector} from "react-redux";
 import store from "../../App/Store";
 import CDSlice, {CDDetails} from "../../Slices/CDSlice";
+import {useEffect} from "react";
 
 const CDView = (prop) => {
 	// prop
@@ -12,15 +13,20 @@ const CDView = (prop) => {
 
 	// dispatch async action
 	const dispatch = useDispatch();
-	dispatch(CDDetails(id));
+	// Todo Why make two or three request?
+	useEffect(() => {
+		dispatch(CDDetails(id));
+	}, []);
+
 
 	// select cd details
-	const Details = useSelector((state) => state.CDSlice.cdDetails)
+	const Details = useSelector((state) => state.CDSlice.details)
 	console.log(Details)
 
 	return(
 		<div className={"flex flex-row flex-nowrap justify-center overflow-x-auto w-auto h-screen macchiato"}>
-
+			<div>
+			</div>
 		</div>
 	)
 }
